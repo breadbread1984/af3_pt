33f7963860e3cdfd559c0ea4908796e4324449ca
williamxieigs <yi.xie@igsl-group.com>
2025-10-28 09:51:09 +0800

    add comment in training script

--- train.py
+++ train.py
@@ -12,13 +12,12 @@ def add_options():
   flags.DEFINE_string("ckpt", default = None, help = 'resume from existing checkpoint')
 
 def main(unused_argv):
-  # 从 YAML 配置文件创建训练器
+  # some pdb sample cannot be preprocessed. refer to https://github.com/lucidrains/alphafold3-pytorch/issues/296
+  # to prevent the exception in preprocessing interrupt training loop, we commit https://github.com/breadbread1984/af3_pt/commit/0a383cf660ed33739289e730c76d6cf9695d765b
   config_path = './tests/configs/trainer_with_pdb_dataset.yaml' if FLAGS.weighted_sampling == False else \
                 './tests/configs/trainer_with_pdb_dataset_and_weighted_sampling.yaml'
-  # NOTE: github.com/lucidrains/alphafold3-pytorch/blob/main/tests/test_trainer.py#L166
   trainer = create_trainer_from_yaml(config_path)
   if FLAGS.ckpt is not None: trainer.load(FLAGS.ckpt)
-  # 训练模型
   trainer()
   print("Training finished.")
 
